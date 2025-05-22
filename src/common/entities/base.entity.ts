28ad51bb704a415b098851a159b85cab149dc9ea
Pedro Evangelista <pedroevangelistabrasil@gmail.com>
2025-05-22 19:17:59 -0300

    Feat: Documentação do swagger para as entidades

--- src/common/entities/base.entity.ts
+++ src/common/entities/base.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   PrimaryGeneratedColumn,
   Column,
@@ -6,15 +7,19 @@ import {
 } from 'typeorm';
 
 export abstract class BaseEntity {
+  @ApiProperty({ description: 'ID da entidade', type: 'string' })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty({ description: 'Status de atividade da entidade', type: 'boolean' })
   @Column({ type: 'boolean', default: true })
   active: boolean;
 
+  @ApiProperty({ description: 'Data de criação da entidade', type: 'string' })
   @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   createDateTime: Date;
 
+  @ApiProperty({ description: 'Data da última alteração da entidade', type: 'string' })
   @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   lastChangedDateTime: Date;
 }
--- src/modules/clients/entities/client.entity.ts
+++ src/modules/clients/entities/client.entity.ts
@@ -3,21 +3,28 @@ import { BaseEntity } from '../../../common/entities/base.entity';
 import { Plan } from '../../../common/enums/Plan.enum';
 import { Document } from '../../../common/enums/Document.enum';
 import { Role } from '../../../common/enums/Role.enum';
+import { ApiProperty } from '@nestjs/swagger';
+import { Exclude } from 'class-transformer';
 
 @Entity('clients')
 export class Client extends BaseEntity {
+  @ApiProperty({ description: 'Nome do cliente' , type: 'string'})
   @Column()
   name: string;
 
+  @ApiProperty({ description: 'Documento do cliente CPF/CNPJ' , type: 'string'})
   @Column({ unique: true })
   documentId: string; // CPF ou CNPJ
 
+  @ApiProperty({ description: 'Tipo do documento do cliente CPF/CNPJ',  type: 'string'})
   @Column({ type: 'enum', enum: Document })
   documentType: Document; // 'CPF' ou 'CNPJ'
 
+  @ApiProperty({ description: 'Plano selecionado', type: 'string' })
   @Column({ type: 'enum', enum: Plan })
   planType: Plan; // 'prepaid' ou 'postpaid'
 
+  @ApiProperty({ description: 'Saldo para clientes pré-pago', type: 'number' })
   @Column({
     type: 'decimal',
     precision: 10,
@@ -27,6 +34,7 @@ export class Client extends BaseEntity {
   })
   balance: number; // Saldo (para pré-pago)
 
+  @ApiProperty({ description: 'Limite para clientes pós-pago', type: 'number' })
   @Column({
     type: 'decimal',
     precision: 10,
@@ -36,6 +44,7 @@ export class Client extends BaseEntity {
   })
   limit: number; // Limite (para pós-pago)
 
+  @Exclude()
   @Column({
     type: 'enum',
     enum: Role,
